7dc55bb93c67a491483817af8dfb9ffee506fbe3
GytisKupinas <gytis.kupinas@gmail.com>
2021-10-17 22:55:11 +0300

    RoutesAndPath

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
 
@@ -7,6 +7,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ListComponent } from './components/list/list.component';
 import { CreateComponent } from './components/create/create.component';
 import { EditComponent } from './components/edit/edit.component';
+
+const routes: Routes = [
+{ path: 'create', component: CreateComponent},
+{ path: 'edit/:id', component: EditComponent},
+{ path: 'list', component: ListComponent},
+{ path: '', redirectTo: 'list', pathMatch: 'full'}
+];
+
 
 @NgModule({
   declarations: [
